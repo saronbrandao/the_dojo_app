f2f9990319101d651fb0d32bd2bc5deb79e1c294
Saron <saronbrandao@gmail.com>
2022-12-29 03:01:56 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -10,31 +10,35 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/Navbar';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/projects/:id">
-            <Project />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 

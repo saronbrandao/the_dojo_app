3e958dbc374f122808906d1ef87d1c48f6242787
Saron <saronbrandao@gmail.com>
2022-12-30 19:38:06 +0100

    updates

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAaLjD06tc0YIpa3vct7gGgbPif3tCsjAE',
@@ -18,8 +19,9 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 // styles
 import './Signup.css';
@@ -8,9 +9,39 @@ const Signup = () => {
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+  const { signup, error, isPending } = useSignup();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (!selected) {
+      setThumbnailError('Please select a file');
+      return;
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image');
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb');
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log('thumbnail updated');
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>email:</span>
@@ -41,9 +72,16 @@ const Signup = () => {
       </label>
       <label>
         <span>profile thumbnail:</span>
-        <input required type="file" />
+        <input required type="file" onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
-      <button className="btn">Sign up</button>
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };

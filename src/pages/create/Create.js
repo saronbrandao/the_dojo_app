8aa2ab621dd666aeb33aaa9ab72a82fdb1f7e08f
Saron <saronbrandao@gmail.com>
2023-01-03 17:00:54 +0100

    master

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,78 @@
+import { useState } from 'react';
+import Select from 'react-select';
+
+// styles
 import './Create.css';
 
+// bellow you see the options that would be present in a <option> tag,
+// but we will use a react hook for this instead. For example:
+// <option value="">Label</option>
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+];
+
 const Create = () => {
-  return <div>Create</div>;
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [category, setCategory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate, category.value);
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* assignee select here */}
+        </label>
+
+        <button className="btn"> Add Project</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;

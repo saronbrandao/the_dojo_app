f37648c07d7fffb3d52aa66947cfbf79c10be47e
Saron <saronbrandao@gmail.com>
2023-01-10 18:35:48 +0100

    updtes

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -20,7 +20,19 @@ const firestoreReducer = (state, action) => {
         error: null,
       };
     case 'DELETED_DOCUMENT':
-      return { isPending: false, document: null, success: true, error: null };
+      return {
+        isPending: false,
+        document: null,
+        success: true,
+        error: null,
+      };
+    case 'UPDATED_DOCUMENT':
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
     case 'ERROR':
       return {
         isPending: false,
@@ -78,9 +90,26 @@ export const useFirestore = (collection) => {
     }
   };
 
+  // update documents
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: 'IS_PENDING' });
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates);
+      dispatchIfNotCancelled({
+        type: 'UPDATED_DOCUMENT',
+        payload: updatedDocument,
+      });
+      return updateDocument;
+    } catch (err) {
+      dispatch({ type: 'ERROR', payload: err.message });
+      return null;
+    }
+  };
+
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { addDocument, deleteDocument, response };
+  return { addDocument, deleteDocument, updateDocument, response };
 };
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -21,7 +21,7 @@ const Project = () => {
   return (
     <div className="project-details">
       <ProjectSummary project={document} />
-      <ProjectComments />
+      <ProjectComments project={document} />
     </div>
   );
 };
